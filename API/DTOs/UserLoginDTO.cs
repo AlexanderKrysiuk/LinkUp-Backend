51be17b377be8278bd750396fab763277cc373be
Alexander Krysiuk <31800082+AlexanderKrysiuk@users.noreply.github.com>
2023-08-02 19:37:00 +0200

    Delete API directory

--- API/DTOs/UserLoginDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API.DTOs
-{
-    public class UserLoginDTO
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-
-    }
-}
