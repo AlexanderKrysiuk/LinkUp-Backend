9d05d55b465ee549f4e58578b2813d99fd79d79b
marcioszka <monkiewicz.marta@gmail.com>
2023-08-04 20:38:34 +0200

    Implement test projects

---
+++ DBTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using DBTest.Models;
+using LinkUp.Data;
+
+AppDbContext db = new AppDbContext();
+
+var users = db.Users.ToList();
+
+foreach (var user in users)
+{
+    Console.WriteLine(user.Name, user.Email, user.Id, '\n');
+}
+
+Console.WriteLine("Done!");
