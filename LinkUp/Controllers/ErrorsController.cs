23fda980e97c2a592ffe2ec2a081066fb2dc9cea
marcioszka <monkiewicz.marta@gmail.com>
2023-08-05 16:37:05 +0200

    Add http attribute

--- LinkUp/Controllers/ErrorsController.cs
+++ LinkUp/Controllers/ErrorsController.cs
@@ -5,6 +5,7 @@ namespace LinkUp.Controllers;
 public class ErrorController : ControllerBase
 {
     [Route("/error")]
+    [HttpGet]
     public IActionResult Error()
     {
         return Problem();
