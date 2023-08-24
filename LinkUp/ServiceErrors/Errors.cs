fcce9ddbe51c39e59d413cdd1f706d6994c7a76b
marcioszka <monkiewicz.marta@gmail.com>
2023-08-24 12:35:32 +0200

    Add user errors

--- LinkUp/ServiceErrors/Errors.cs
+++ LinkUp/ServiceErrors/Errors.cs
@@ -20,18 +20,18 @@ public static class Errors
         );
     }
 
-    public static class Client
+    public static class User
     {
         public static Error InvalidName => Error.Validation(
-            code: "Client.InvalidName",
+            code: "User.InvalidName",
             description: $"Name must contain two parts"
         );
         public static Error InvalidEmail => Error.Validation(
-            code: "Client.InvalidEmail",
+            code: "User.InvalidEmail",
             description: "Mail must contain @"
         );
         public static Error NotFound => Error.NotFound(
-            code: "Client.NotFound",
+            code: "User.NotFound",
             description: "Client not found"
         );
     }
