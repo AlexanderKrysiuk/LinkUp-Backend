e0ef5fe47ce99633096c31460cd99a779ac67599
marcioszka <monkiewicz.marta@gmail.com>
2023-08-28 15:44:30 +0200

    Add DTO for user registration

---
+++ DTOs/UserForRegistationDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkUpBackend.DTOs;
+
+public class UserForRegistationDTO
+{
+    [Required]
+    [MaxLength(256)]
+    public string UserName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(256)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(64)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    public ICollection<string> Roles { get; set; } = new List<string>();
+}
+
