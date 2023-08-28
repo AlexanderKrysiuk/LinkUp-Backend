40f84cbcdf667cbd2d5c8a076379a047ab678d3f
marcioszka <monkiewicz.marta@gmail.com>
2023-08-28 13:02:39 +0200

    Add Role

--- Infrastructure/AppDbContext.cs
+++ Infrastructure/AppDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LinkUpBackend.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LinkUpBackend.Infrastructure
 {
-    public class AppDbContext : IdentityDbContext
+    public class AppDbContext : IdentityDbContext<User, Role, string>
     {
+        public override DbSet<Role> Roles => Set<Role>();
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
         { }
 
