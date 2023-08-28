4080069ad5138a70d53dc9e62bf074b21e03216e
marcioszka <monkiewicz.marta@gmail.com>
2023-08-28 14:40:15 +0200

    Add UsersController

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkUpBackend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+//[Authorize]
+public class UsersController : ControllerBase
+{
+    public UsersController()
+    {
+
+    }
+
+
+    [HttpOptions("register")]
+    [ResponseCache(CacheProfileName = "NoCache")]
+    [AllowAnonymous]
+    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> Register()
+    {
+        return Accepted();
+    }
+
+
+    [HttpOptions("login")]
+    [ResponseCache(CacheProfileName = "NoCache")]
+    [AllowAnonymous]
+    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Login()
+    {
+        return Accepted();
+    }
+
+
+    [HttpOptions("logout")]
+    [ResponseCache(CacheProfileName = "NoCache")]
+    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Logout()
+    {
+        return Accepted();
+    }
+
+    [HttpOptions("access-denied")]
+    [ResponseCache(CacheProfileName = "NoCache")]
+    [AllowAnonymous]
+    public async Task<IActionResult> AccessDenied()
+    {
+        return Accepted();
+    }
+
+}
+
