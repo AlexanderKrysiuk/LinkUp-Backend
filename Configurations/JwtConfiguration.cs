9ee4af18e6e2dbfc58011300fd0415a13f5e11f7
marcioszka <monkiewicz.marta@gmail.com>
2023-08-28 20:55:56 +0200

    Add null forgiving operator

---
+++ Configurations/JwtConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkUpBackend.Configurations;
+
+public class JwtConfiguration
+{
+    public static readonly string SectionName = "Authentication:Jwt";
+
+    [Required]
+    [MaxLength(64)]
+    public string Issuer { get; set; } = default!;
+
+    [Required]
+    [MaxLength(64)]
+    public string Audience { get; set; } = default!;
+
+    [Required]
+    [MaxLength(256)]
+    public string SigningKey { get; set; } = default!;
+}
