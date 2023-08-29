5945aa524781a72b27d9c62a304b378892b8c6a5
marcioszka <monkiewicz.marta@gmail.com>
2023-08-29 14:28:29 +0200

    Fix buf with JwtConfiguration dependency injection

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -26,10 +27,10 @@ public class UsersController : ControllerBase
 
     //private readonly ILogger<UsersController> _logger;
 
-    public UsersController(UserManager<User> userManager, RoleManager<Role> roleManager, SignInManager<User> signInManager, JwtConfiguration jwtConfiguration)
+    public UsersController(UserManager<User> userManager, RoleManager<Role> roleManager, SignInManager<User> signInManager, IOptions<JwtConfiguration> jwtConfiguration)
     {
         _userManager = userManager;
-        _jwtConfiguration = jwtConfiguration;
+        _jwtConfiguration = jwtConfiguration.Value;
         _signInManager = signInManager;
     }
 
