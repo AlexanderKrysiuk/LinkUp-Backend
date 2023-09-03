ac80525a2a5f7063c89bb40b9da156051c97ab7a
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2023-09-03 22:37:14 +0200

    Add ErrorTypes, overload Problem()

---
+++ ServiceErrors/Error.cs
@@ -0,0 +1,22 @@
+﻿using ErrorOr;
+
+namespace LinkUpBackend.ServiceErrors;
+
+public static class Errors
+{
+    public static class User
+    {
+        public static Error InvalidName => Error.Validation(
+            code: "User.InvalidName",
+            description: $"Name must not be longer than 256 characters"
+        );
+        public static Error InvalidEmail => Error.Validation(
+            code: "User.InvalidEmail",
+            description: "Mail must contain @"
+        );
+        public static Error NotFound => Error.NotFound(
+            code: "User.NotFound",
+            description: "User not found"
+        );
+    }
+}
