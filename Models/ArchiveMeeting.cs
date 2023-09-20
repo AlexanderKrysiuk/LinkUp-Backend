d1056d75ba50e035c2e4452fbeb5bdd9c7d40ddd
marcioszka <monkiewicz.marta@gmail.com>
2023-09-20 10:59:50 +0200

    Add entity to store archived meetings

---
+++ Models/ArchiveMeeting.cs
@@ -0,0 +1,9 @@
+﻿namespace LinkUpBackend.Models
+{
+    public class ArchiveMeeting
+    {
+        public Guid Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string? Description { get; set; }
+    }
+}
