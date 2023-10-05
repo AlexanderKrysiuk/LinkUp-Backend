8f79e7ef9ac91ca959e4f2ceb71e642237dfc26f
marcioszka <monkiewicz.marta@gmail.com>
2023-10-06 00:29:03 +0200

    Revert "Add docs for user and role models"
    This reverts commit 159923e7453c81bafc499a229d3cbd66a657df98.

--- Models/User.cs
+++ Models/User.cs
@@ -5,72 +5,46 @@ using Microsoft.AspNetCore.Identity;
 using System.Text.RegularExpressions;
 
 
-namespace LinkUpBackend.Models;
-
-/// <summary>
-/// Class representing user profile based on Identity
-/// </summary>
-public class User : IdentityUser
+namespace LinkUpBackend.Models
 {
-    /// <summary>
-    /// Creates a new user based on the provided registration data
-    /// </summary>
-    /// <param name="userToRegister">The registration data for the new user</param>
-    /// <returns>
-    /// A result that represents either a list of validation errors if the registration data is invalid,
-    /// or a User object if the registration is successful
-    /// </returns>
-    public static ErrorOr<User> Create(UserToRegisterDTO userToRegister)
+    public class User : IdentityUser
     {
-        List<Error> errors = new();
-        if (!IsTwoPartName(userToRegister.Username))
+        public static ErrorOr<User> Create(UserToRegisterDTO userToRegister)
         {
-            errors.Add(Errors.User.InvalidName);
+            List<Error> errors = new();
+            if (!IsTwoPartName(userToRegister.Username))
+            {
+                errors.Add(Errors.User.InvalidName);
+            }
+            if (!IsEmail(userToRegister.Email))
+            {
+                errors.Add(Errors.User.InvalidEmail);
+            }
+            if (!isPasswordValid(userToRegister.Password))
+            {
+                errors.Add(Errors.User.InvalidPassword);
+            }
+            if (errors.Count > 0)
+            {
+                return errors;
+            }
+            return new User() { Email = userToRegister.Email, UserName = userToRegister.Username };
+
         }
-        if (!IsEmail(userToRegister.Email))
+
+        static bool IsTwoPartName(string name)
         {
-            errors.Add(Errors.User.InvalidEmail);
+            return name.Count(a => a == ' ') == 1;
         }
-        if (!isPasswordValid(userToRegister.Password))
+
+        static bool isPasswordValid(string password)
         {
-            errors.Add(Errors.User.InvalidPassword);
+            return Regex.IsMatch(password, @"^(?=.*\d)(?=.*[A-Z])(?=.*[\W_]).{8,}$");
         }
-        if (errors.Count > 0)
+
+        static bool IsEmail(string email)
         {
-            return errors;
+            return Regex.IsMatch(email, @"^[a-zA-Z0-9]+([.\-_]?[a-zA-Z0-9])*@[a-zA-Z0-9]+(-?[a-zA-Z0-9])*\.[a-zA-Z]{2,}$");
         }
-        return new User() { Email = userToRegister.Email, UserName = userToRegister.Username };
-
-    }
-
-    /// <summary>
-    /// Checks if the provided name consists of two parts separated by a space
-    /// </summary>
-    /// <param name="name">The name to be checked</param>
-    /// <returns>True if the name consists of two parts; otherwise, false</returns>
-    static bool IsTwoPartName(string name)
-    {
-        return name.Count(a => a == ' ') == 1;
-    }
-
-    /// <summary>
-    /// Validates a password based on the specified criteria
-    /// </summary>
-    /// <param name="password">The password to be validated.</param>
-    /// <returns>True if the password is valid; otherwise, false.</returns>
-    static bool isPasswordValid(string password)
-    {
-        return Regex.IsMatch(password, @"^(?=.*\d)(?=.*[A-Z])(?=.*[\W_]).{8,}$");
-    }
-
-    /// <summary>
-    /// Check if the provided string is a valid email address.
-    /// </summary>
-    /// <param name="email">The string to be validated as an email address</param>
-    /// <returns>True if the string is a valid email address; otherwise, false</returns>
-
-    static bool IsEmail(string email)
-    {
-        return Regex.IsMatch(email, @"^[a-zA-Z0-9]+([.\-_]?[a-zA-Z0-9])*@[a-zA-Z0-9]+(-?[a-zA-Z0-9])*\.[a-zA-Z]{2,}$");
     }
 }
